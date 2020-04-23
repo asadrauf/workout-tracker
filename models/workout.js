56a9bb139eaef097febbe11e03c9e55895320310
ARauf Git <umiasad2017@gmail.com>
2020-04-22 22:40:57 -0400

    added model workout and mongoose schema for workout model

---
+++ models/workout.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Exercise type is needed"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Exercise type is needed"
+        },
+        duration: {
+          type: Number,
+          required: "Exercise duration is needed in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      //source of info: https://mongoosejs.com/docs/tutorials/virtuals.html
+      virtuals: true
+    }
+  }
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function() {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
